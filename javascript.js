fc844a08affb765e635e6f162f4ce612412d0ea0
student-cpt-146 <student-cpt-146@citi.org.za>
2020-02-14 14:49:44 +0200

    my website

---
+++ javascript.js
@@ -0,0 +1,20 @@
+$('#contact-form') .submit(function(e){
+    var name = document.getElementById('inputName'),
+    email = document.getElementById('inputEmail'),
+    message= document.getElementById('inputMessage');
+
+    if(!name.value || !email.value || !message.value) {
+      alertify.error('please check your entries')
+    } else{
+        $.ajax({
+            url: "https://formspree.io/mziwenkosi830@gmail.com",
+            method: "POST",
+            data: $(this).serialize(),
+            dataType: "json"
+        });
+      e.preventDefault()
+      $(this).get(0).reset()
+      alertify.success('Message sent')
+    }
+})
+
